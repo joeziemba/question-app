28024168821cf42011870c9439e427972eea20ce
Joe Ziemba <jziemba90@gmail.com>
2017-11-27 13:34:45 -0500

    Added Question m/v/c and table

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Question.create(title:"How do I create a controller in Rails?", description:"What is the rails command line tool for creating a controller with all the associated views, migrations, etc?")
+Question.create(title:"Where do the socks go?", description:"Every time I do laundry I get socks with no matches. I know I took them off together, so where'd the other ones go? Do gnomes take them?!")
+Question.create(title:"Why did the storks stop delivering babies?", description:"I mean I know theyre are easier ways of getting babies but I really want one delivered by a bird.")
